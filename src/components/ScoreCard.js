ea85260a09ad90c6a3180c081b979fff2a882ced
JGP345 <jorgeprias345@gmail.com>
2025-02-21 18:18:38 -0600

    ScoreCard

--- src/components/ScoreCard.js
+++ src/components/ScoreCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ScoreCard = ({ aiScore, playerScore }) => (
   <div className="score-card">
-    <div className="score">Mr.....................AI: {aiScore}</div>
+    <div className="score">Mr.AI: {aiScore}</div>
     <div className="score">Player: {playerScore}</div>
   </div>
 );
